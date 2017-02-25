622127c77979d9dc77974d77344d8b36e08a105e
keilsmit <keilsmit@yahoo.com>
2017-02-25 13:56:20 -0500

    first commit for palindrome

--- palindrome/tester.rb
+++ palindrome/tester.rb
@@ -2,12 +2,29 @@ class Tester
   class T1
     def palindrome?(string)
       # first implementation
+      if string.length == 1 || string.length == 0
+        true
+      else
+        if string[0] == string[-1]
+          palindrome?(string[1..-2])
+        else
+          false
+        end
+      end
     end
   end
+
 
   class T2
     def palindrome?(string)
       # second implementation
+      if string.reverse == string
+        true
+      else
+        false
+      end
     end
   end
+
+
 end
